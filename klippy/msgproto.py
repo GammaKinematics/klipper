e2f8a0f706cdf4dcabeaee6d3c5ea715fcd0b095
GammaKinematics <117033687+GammaKinematics@users.noreply.github.com>
2022-11-03 02:16:35 +0100

    remove some logging

--- klippy/msgproto.py
+++ klippy/msgproto.py
@@ -330,7 +330,6 @@ class MessageParser:
         try:
             argparts = dict(arg.split('=', 1) for arg in parts[1:])
             for name, value in argparts.items():
-                logging.info("CGPK msg teared %s -> %s:%s", msgname, name, value)
                 t = mp.name_to_type[name]
                 if t.is_dynamic_string:
                     tval = self._parse_buffer(value)

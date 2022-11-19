8db53a54e91e259c27232748cc4606f330349a7c
GammaKinematics <117033687+GammaKinematics@users.noreply.github.com>
2022-11-19 11:53:05 +0100

    test

--- src/analog_probe.c
+++ src/analog_probe.c
@@ -264,16 +264,16 @@ command_analog_probe_init(uint32_t *args)
 DECL_COMMAND(command_analog_probe_init,
              "analog_probe_init oid=%c clock=%u rest_ticks=%u pin_value=%c");
 
-void
-command_analog_probe_stop(uint32_t *args)
-{
-    return;
-    // struct analog_probe *probe = oid_lookup(args[0], command_config_analog_probe);
-    // sched_del_timer(&probe->time);
-    // gpio_adc_cancel_sample(probe->pin);
-}
-DECL_COMMAND(command_analog_probe_stop,
-             "analog_probe_stop oid=%c");
+// void
+// command_analog_probe_stop(uint32_t *args)
+// {
+//     return;
+//     // struct analog_probe *probe = oid_lookup(args[0], command_config_analog_probe);
+//     // sched_del_timer(&probe->time);
+//     // gpio_adc_cancel_sample(probe->pin);
+// }
+// DECL_COMMAND(command_analog_probe_stop,
+//              "analog_probe_stop oid=%c");
 
 void
 command_analog_probe_home(uint32_t *args)
